c0a435079d7d1bff19fb8d9a525b19cff3ce272e
srrameshin-tech <srrameshin@gmail.com>
2026-06-27 14:21:54 +0530

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'import-reports-v1';
+const ASSETS = ['./index.html', './app.js', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
