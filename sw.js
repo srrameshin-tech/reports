5fb57f07a279b2719ee00d54bb94a5af39a62c31
srrameshin-tech <srrameshin@gmail.com>
2026-06-30 17:55:10 +0530

    Bump service worker cache version to force refresh (attach button not showing)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'import-reports-v2';
+const CACHE_NAME = 'import-reports-v3';
 const ASSETS = ['./index.html', './app.js', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
